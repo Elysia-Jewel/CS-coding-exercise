5ccaada8cd92c86bb773acb2e38e6ad39a90dee0
Elysia-Jewel <84126475+Elysia-Jewel@users.noreply.github.com>
2021-09-01 19:38:52 +0800

    Add files via upload

---
+++ Program.vb
@@ -0,0 +1,17 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Dim num(0 To 9) As Integer
+        Dim largestNum As Integer
+        largestNum = 0
+        For x = 0 To 9
+            Console.Write("Enter number: ")
+            num(x) = Console.ReadLine
+            If num(x) > largestNum Then
+                largestNum = num(x)
+            End If
+        Next
+        Console.WriteLine("Largest value is " & largestNum)
+    End Sub
+End Module
