c121c9fb9bfdc678dd60db860c232b72fda3f3fb
Elysia-Jewel <84126475+Elysia-Jewel@users.noreply.github.com>
2021-09-01 19:34:11 +0800

    Add files via upload

---
+++ Program.vb
@@ -0,0 +1,24 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Dim LineOfText As String
+        Dim input As String
+        Dim NumberOfCars As Integer
+        Dim CarInfo As System.IO.StreamReader
+        CarInfo = New System.IO.StreamReader("C:\Users\Elysia Jewel\OneDrive\Desktop\BuyerCountry.txt")
+        Console.Write("Enter a country code: ")
+        input = Console.ReadLine()
+        Console.WriteLine("List of car code(s) for this country: ")
+        While Not CarInfo.EndOfStream()
+            LineOfText = CarInfo.ReadLine()
+            If Right(LineOfText, 3) = input Then
+                NumberOfCars = NumberOfCars + 1
+                Console.WriteLine(Left(LineOfText, 3))
+            End If
+
+        End While
+        Console.WriteLine("The total number of cars for this country is " & NumberOfCars & ".")
+        CarInfo.Close()
+    End Sub
+End Module
